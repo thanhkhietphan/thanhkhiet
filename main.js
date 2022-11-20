8ad6d8bfe6be2fa5fcbd3d4c9ab87141b1ead859
thanhkhietphan <thankhietphan678@gmail.com>
2022-11-20 11:18:37 +0700

    adsads

---
+++ main.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = ("#b3f542");
+let h1 = document.getElementById("hello");
+h1.style.color = ("#4287f5");
+h1.style.fontSize = ("40px");
+h1.style.textAlign = ("centen");
+let 
